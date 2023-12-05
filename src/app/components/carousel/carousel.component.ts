1e57efe1b289be0377ec5bdfa110e86eb49528d5
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-12-05 16:29:47 -0300

    Images/texts on carousel

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
 })
 export class CarouselComponent {
-
+  list = [
+    {
+      mobileImageSrc: '../../../assets/Mobile/banner1-mobile.png',
+      tabletImageSrc: '../../../assets/Tablet/banner1-tablet.png',
+      desktopImageSrc: '../../../assets/Desktop/banner1-desktop.png',
+      title: '',
+      description: ''
+    },
+    {
+      mobileImageSrc: '../../../assets/Mobile/banner2-mobile.png',
+      tabletImageSrc: '../../../assets/Tablet/banner2-tablet.png',
+      desktopImageSrc: '../../../assets/Desktop/banner2-desktop.png',
+      title: 'COLEÇÃO ATEMPORAL',
+      description: 'Estilo e qualidade para durar.'
+    },
+    {
+      mobileImageSrc: '../../../assets/Mobile/banner3-mobile.png',
+      tabletImageSrc: '../../../assets/Tablet/banner3-tablet.png',
+      desktopImageSrc: '../../../assets/Desktop/banner3-desktop.png',
+      title: 'COLEÇÃO ATEMPORAL',
+      description: 'Alto impacto visual, baixo impacto ambiental!'
+    },
+  ];
 }
