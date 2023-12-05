3ede1fc0e147a65ba5127de6a3b63a302862cdfe
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-12-05 16:48:25 -0300

    Products category images and alingments

--- src/app/components/cards-categoria/cards-categoria.component.ts
+++ src/app/components/cards-categoria/cards-categoria.component.ts
@@ -7,11 +7,59 @@ import { Component } from '@angular/core';
 })
 export class CardsCategoriaComponent {
   list = [
-    { nome: 'Camisetas' },
-    { nome: 'Bolsas' },
-    { nome: 'Calçados' },
-    { nome: 'Calças' },
-    { nome: 'Casacos' },
-    { nome: 'Óculos' }
+    {
+      mobileImageSrc:
+        '../../../assets/Mobile/categorias/categoria-camiseta.png',
+      tabletImageSrc:
+        '../../../assets/Tablet/categorias/categoria-camiseta.png',
+      desktopImageSrc:
+        '../../../assets/Desktop/categorias/categoria-camiseta.png',
+      nome: 'Camisetas',
+    },
+    {
+      mobileImageSrc:
+        '../../../assets/Mobile/categorias/categoria-bolsas.png',
+      tabletImageSrc:
+        '../../../assets/Tablet/categorias/categoria-bolsas.png',
+      desktopImageSrc:
+        '../../../assets/Desktop/categorias/categoria-bolsa.png',
+      nome: 'Bolsas',
+    },
+    {
+      mobileImageSrc:
+        '../../../assets/Mobile/categorias/categoria-calcados.png',
+      tabletImageSrc:
+        '../../../assets/Tablet/categorias/categoria-calcados.png',
+      desktopImageSrc:
+        '../../../assets/Desktop/categorias/categoria-calcados.png',
+      nome: 'Calçados',
+    },
+    {
+      mobileImageSrc:
+        '../../../assets/Mobile/categorias/categoria-calcas.png',
+      tabletImageSrc:
+        '../../../assets/Tablet/categorias/categoria-calcas.png',
+      desktopImageSrc:
+        '../../../assets/Desktop/categorias/categoria-calcas.png',
+      nome: 'Calças',
+    },
+    {
+      mobileImageSrc:
+        '../../../assets/Mobile/categorias/categoria-casacos.png',
+      tabletImageSrc:
+        '../../../assets/Tablet/categorias/categoria-casacos.png',
+      desktopImageSrc:
+        '../../../assets/Desktop/categorias/categoria-casacos.png',
+      nome: 'Casacos',
+    },
+    {
+      mobileImageSrc:
+        '../../../assets/Mobile/categorias/categoria-oculos.png',
+      tabletImageSrc:
+        '../../../assets/Tablet/categorias/categoria-oculos.png',
+      desktopImageSrc:
+        '../../../assets/Desktop/categorias/categoria-oculos.png',
+      nome: 'Óculos',
+    },
   ];
 }
